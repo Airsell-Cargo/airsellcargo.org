fda6baf2cf5f61574065199803444e953d201d14
Hamse Dahir <airsellcargo@gmail.com>
2026-06-04 23:18:04 +0300

    Create track.php

---
+++ track.php
@@ -0,0 +1,9 @@
+<section id="tracking" class="quick-track">
+  <h2>Quick Tracking</h2>
+  <div class="track-box">
+    <form action="track.php" method="POST">
+      <input type="text" name="mawb" placeholder="Enter MAWB Number (e.g., 071-12345678)" class="track-input" required>
+      <button type="submit" class="track-btn">Track Now</button>
+    </form>
+  </div>
+</section>
